a2020b78508f99ce5682f37323b22e8ed9bc63ce
Edmund Chong Tze Wei <7440820@gmail.com>
2021-02-25 17:04:06 +1100

    Update_1
    >> EntryController.cs
    >> Changed the return type of Search to ActionResult
    >> Added the GetAllEntries() skeleton
    >> Added a home redirect method

--- TransactionEntry/Controllers/EntryController.cs
+++ TransactionEntry/Controllers/EntryController.cs
@@ -9,10 +9,22 @@ namespace TransactionEntry.Controllers
     public class EntryController : Controller
     {
         // GET: Entry
-        public string Search(string query)
+        public ActionResult Search(string query)
         {
             var input = Server.HtmlEncode(query);
-            return input;
+            return Content(input);
+        }
+        public string GetAllEntries()
+        {
+            return @"<ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            </ul>";
+        }
+        public ActionResult ReturnHome()
+        {
+            return RedirectToAction("Index", "Home");
         }
     }
 }
